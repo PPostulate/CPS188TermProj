7ca26ccf1e635673e278ad95375243dd94215032
Matt <matt.yasui@gmail.com>
2024-03-16 18:02:45 -0400

    Fixed tDataArray

--- Main.c
+++ Main.c
@@ -4,23 +4,23 @@
 // Yo if you see this commit then I am officially a git bash demon. Like  this was probably the most complicated thing that I've ever done
 // This is a massive change can you like work?
 // Some changes here
-void getcTitle (char *cTitleArr[], FILE* gloTempData)
+void getcTitle (char *cTitleArr[], FILE* gloTempData, char *token, FILE *out)
 {
-	char cTitle[300], *token;
+	char cTitle[300];
 	fgets(cTitle, 300, gloTempData);
 	token = strtok(cTitle, ",");
 	for (short i = 0; token != NULL; i++)
 	{
 		cTitleArr[i] = token;
-		printf("%s\n", cTitleArr[i]);
+		//fprintf(out, "%s\n", cTitleArr[i]);
 		token = strtok (NULL, ",");
 	}
 }
 
-void getTempData (char *dArr[], float tDataArr[][8], FILE *gloTempData)
+void getTempData (char *dArr[], float tDataArr[][8], FILE *gloTempData, char *token, FILE *out)
 {
-	char line[100], *token;
-	for (short i = 0; fgets(line, 100, gloTempData); i++)
+	char line[200];
+	for (short i = 0; fgets(line, 200, gloTempData); i++)
 	{
 		token = strtok(line, ",");
 		for (short j = 0; token != NULL; j++) 
@@ -28,13 +28,13 @@ void getTempData (char *dArr[], float tDataArr[][8], FILE *gloTempData)
 			switch (j)
 			{
 				case 0:
-					dArr[i] = token;
-					printf("%s\n", dArr[i]);
+					//dArr[i] = token;
+					fprintf(out, "%s\n", dArr[i]);
 					break;
 				
 				default:
 					tDataArr[i][j-1] = atof(token);
-					printf("%f\n", tDataArr[i][j-1]);
+					//fprintf(out, "i:%hd j:%hd val:%f\n", i, j-1, tDataArr[i][j-1]);
 					break;
 					
 			}
@@ -46,11 +46,13 @@ void getTempData (char *dArr[], float tDataArr[][8], FILE *gloTempData)
 int main (void)
 {
 	FILE *gloTempData;
+	FILE *out; 
 	gloTempData = fopen("GlobalTemperatures.csv","r");
+	out = fopen("output.txt", "w");
 	/* tDataArr contains the numerical data in the spreadsheet stored as
 	 * floats. The array is two dimensional; [row#][column#]. Note that
 	 * the row # in the array doesn't match the spreadsheet row #.*/
-	
+	char *token = "thing";
 	float tDataArr[3200][8];
 	
 	/* dArr contains the date column of the spreadsheet. The dates are
@@ -58,11 +60,11 @@ int main (void)
 	 * titles are stored as strings.*/ 
 	char *dArr[3200], *cTitleArr[9];
 	
-	getcTitle(cTitleArr, gloTempData);
-	getTempData(dArr, tDataArr, gloTempData);
+	getcTitle(cTitleArr, gloTempData, token, out);
+	getTempData(dArr, tDataArr, gloTempData, token, out);
 	//for (short i = 0; i < 3200; i++)
 	//{
-		//printf("%s\n", dArr[i]);
+		//fprintf(out, "%s\n", dArr[i]);
 	//}
 	
 	
