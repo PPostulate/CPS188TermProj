b89cffd23efa17767e3ad47c641623c6df02fd18
Matt <matt.yasui@gmail.com>
2024-03-30 17:41:23 -0400

    Progress on q11

--- Main.c
+++ Main.c
@@ -349,23 +349,31 @@ void Q10Format(float tDataArr[][8])
 	}
 }
 
-Q11OceanLandCalc(float tDataArr[][8], float oceanLandAvgTemp[])
+void Q11OceanLandCalc(float tDataArr[][8], float oceanLandAvgTemp[256])
 {
-
+	for (short i = 1200, j = 0; i < DataLen; i += 12, j++)
+	{
+		for (short k = 0; k < 12; k++)
+		{
+			oceanLandAvgTemp[j] += tDataArr[i + k][6];
+		}
+		oceanLandAvgTemp[j] /= 12;
+		//printf("%-10hd%-10hd%f\n", i, j, oceanLandAvgTemp[j]);
+	}
 }
 
 
 //Question 11
 void Q11Format(float tDataArr[][8], double landAvgTemp[])
 {
-	float oceanLandAvgTemp[256];
-
+	float oceanLandAvgTemp[256] = { 0 };
+	Q11OceanLandCalc(tDataArr, oceanLandAvgTemp);
 }
 
 
 int main (void)
 {
-	#pragma region Data Input/ Declaration
+#pragma region Data Input/ Declaration
 		FILE* gloTempData;
 		FILE* out;
 		gloTempData = fopen("GlobalTemperatures.csv", "r");
@@ -386,7 +394,7 @@ int main (void)
 		}*/
 		getcTitle(cTitleArr, gloTempData, token, out);
 		getTempData(Dates, TemperatureData, gloTempData, token, out);
-	#pragma endregion
+#pragma endregion
 
 #pragma region FinishedQuestions
 	// Question 1
@@ -396,22 +404,22 @@ int main (void)
 	/* double Temperatures1760 = Question2_CalculateLandAverageTemperature(TemperatureData,Dates,"1760","1800");
 	 printf("%lf\n",Temperatures1760); */
 
-		// double Temperatures1800 = Question2_CalculateLandAverageTemperature(TemperatureData,Dates,"1800","1900");
-		// printf("%lf\n",Temperatures1800);
+	// double Temperatures1800 = Question2_CalculateLandAverageTemperature(TemperatureData,Dates,"1800","1900");
+	// printf("%lf\n",Temperatures1800);
 
-		// double Temperatures1900 = Question2_CalculateLandAverageTemperature(TemperatureData,Dates,"1900","2000");
-		//  printf("%lf\n",Temperatures1900);
+	// double Temperatures1900 = Question2_CalculateLandAverageTemperature(TemperatureData,Dates,"1900","2000");
+	//  printf("%lf\n",Temperatures1900);
 
-		// double Temperatures2000 = Question2_CalculateLandAverageTemperature(TemperatureData,Dates,"2000","2016");
-		// printf("%lf\n",Temperatures2000);
+	// double Temperatures2000 = Question2_CalculateLandAverageTemperature(TemperatureData,Dates,"2000","2016");
+	// printf("%lf\n",Temperatures2000);
 
 	// Question 3
 	//Question3_CalculateMonthlyAverageTemperatures(TemperatureData, monthAvgTemp);
 	// Question 4 + Question 5
 	//Question4_FindHottestAndColdestMonth(TemperatureData,Dates);
 
-		//Question 6
-		//Question6_GenerateGNUPlotFromData(TemperatureData,Dates,LandAverageTemperatures);
+	//Question 6
+	//Question6_GenerateGNUPlotFromData(TemperatureData,Dates,LandAverageTemperatures);
 
 	//Question 8
 	//Q8AverageTemp(TemperatureData);
@@ -425,11 +433,11 @@ int main (void)
 #pragma endregion
 
 	#pragma region In Progress Questions
-		// Question 7
-		Question7_PlotTemperaturesFrom19To20Century(TemperatureData,Dates);
+	//Question 7
+	//Question7_PlotTemperaturesFrom19To20Century(TemperatureData,Dates);
 
 	//Question 11
-	//Q11Format(TemperatureData, LandAverageTemperatures);
+	Q11Format(TemperatureData, LandAverageTemperatures);
 
 	#pragma endregion
 
